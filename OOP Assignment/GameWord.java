103934a901fe50ec115f43f267d64e78576192a9
Kevy wevy <123706048+Kevinfied@users.noreply.github.com>
2023-10-18 20:10:11 -0400

    Update GameWord.java

--- OOP Assignment/GameWord.java
+++ OOP Assignment/GameWord.java
@@ -7,18 +7,50 @@
 //============================================================================
 
 
+
 import java.util.*;
 public class GameWord {
 
     private String word;
 
+    String contents = this.word;
+
+    /*
+    Double Letter - 12
+    Triple Letter - 13
+    Double Word   - 21
+    Triple Word   - 31
+                   W L
+     */
+
+
+    final int [][]board = {
+            {31,11,11,12,11,11,11,31,11,11,11,12,11,11,31},
+            {11,21,11,11,11,13,11,11,11,13,11,11,11,21,11},
+            {11,11,21,11,11,11,21,11,12,11,11,11,21,11,11},
+            {12,11,11,21,11,11,11,12,11,11,11,21,11,11,12},
+            {11,11,11,11,21,11,11,11,11,11,21,11,11,11,11},
+            {11,13,11,11,11,13,11,11,11,13,11,11,11,13,11},
+            {11,11,12,11,11,11,12,11,12,11,11,11,12,11,11},
+            {31,11,11,12,11,11,11,21,11,11,11,12,11,11,31},
+            {11,11,12,11,11,11,12,11,21,11,11,11,12,11,11},
+            {11,13,11,11,11,13,11,11,11,21,11,11,11,13,11},
+            {11,11,11,11,21,11,11,11,11,11,21,11,11,11,11},
+            {12,11,11,21,11,11,11,12,11,11,11,21,11,11,12},
+            {11,11,21,11,11,11,12,11,12,11,11,11,21,11,11},
+            {11,21,11,11,11,13,11,11,11,13,11,11,11,21,11},
+            {31,11,11,12,11,11,11,31,11,11,11,12,11,11,31}
+    };
+
 
 
 
     public GameWord(String word) {
+
         this.word = word;
     }
 
+    
 
 
     public String reverse() {
@@ -44,28 +76,38 @@ public class GameWord {
 
             return anagram;
         }
-
-
-
-
     }
 
     public int pointValue(int x, int y, int direction) {
 //        direction == 1 means RIGHT
 //        direction == 2 means DOWN
 
+// 🤦
+//        Set<Character>onePoints = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u'));
+//        Set<Character>twoPoints = new HashSet<Character>(Arrays.asList('d', 'g'));
+//        Set<Character>threePoints = new HashSet<Character>(Arrays.asList('b', 'c', 'm', 'p'));
+//        Set<Character>fourPoints = new HashSet<Character>(Arrays.asList('f', 'h', 'v', 'w', 'y'));
+//        Set<Character>fivePoints = new HashSet<Character>(Arrays.asList('k'));
+//        Set<Character>eightPoints = new HashSet<Character>(Arrays.asList('j', 'x'));
+//        Set<Character>tenPoints = new HashSet<Character>(Arrays.asList('q', 'z'));
 
-        Set<Character>onePoints = new HashSet<Character>(Arrays.asList('a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u'));
-        Set<Character>twoPoints = new HashSet<Character>(Arrays.asList('d', 'g'));
-        Set<Character>threePoints = new HashSet<Character>(Arrays.asList('b', 'c', 'm', 'p'));
-        Set<Character>fourPoints = new HashSet<Character>(Arrays.asList('f', 'h', 'v', 'w', 'y'));
-        Set<Character>fivePoints = new HashSet<Character>(Arrays.asList('k'));
-        Set<Character>eightPoints = new HashSet<Character>(Arrays.asList('j', 'x'));
-        Set<Character>tenPoints = new HashSet<Character>(Arrays.asList('q', 'z'));
-
+        int[] vals = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1,
+                        1, 4, 4, 8, 4, 10};
         int points = 0;
 
+        if (direction == 1) {
+            for (int i = 0; i < word.length(); i++) {
+                points += vals[word.charAt(i) - 'a'] * board[x][y + i];
+                System.out.println(board[x+ i][y]);
+            }
+        }
 
+        else if (direction == 2) {
+            for (int i = 0; i < word.length(); i++) {
+                points += vals[word.charAt(i) - 'a'] * board[x + i][y];
+
+            }
+        }
 
         return points;
     }
@@ -94,7 +136,11 @@ public class GameWord {
         return perm;
     }
 
+    @Override
+    public String toString() {
+        return contents;
+    }
+
 
 }
-
 
