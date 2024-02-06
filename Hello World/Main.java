73ce362246bd0fd1edd46918923fbbaf27fb17da
Kevy wevy <123706048+Kevinfied@users.noreply.github.com>
2024-02-06 12:23:19 -0500

    Create Main.java

---
+++ Hello World/Main.java
@@ -0,0 +1,45 @@
+/*
+Java is an object orientated programming language. This means that our programs are organized into classes and objects.
+Java is also device-independent. WIth languages like C, you need to compile your program for the machine that your program
+is running on.
+
+Java is statically-typed language. This means ALL variables need a type, and they can only store values of that type.
+
+
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+
+
+
+    }
+
+
+
+}
+
+/*
+Class Main - ALL programs are also classes. In Replit, class must be Main.
+main - This is a method (function in a class).
+in your Main class, you should have a main method. The main method always gets ran first.
+public - You can make variables amd methods public or private.
+
+Static - "belongs to the class".
+
+void - In Java, you must declare what type your method will return. void means that it does not return anything.
+
+String[] args - We can pass in parameters to our program when we run it. This is most common when we run from the
+command line.
+
+System.out.println() - print, and add a newline character.
+
+ */
+
+
+
